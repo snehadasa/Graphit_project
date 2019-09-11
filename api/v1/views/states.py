bbe10d71c44522ca546e9d08c7cd761ebdd34681
David Kwan <dwkwan1989@gmail.com>
2019-09-11 01:15:44 +0000

    Add PUT and POST default actions.

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -18,7 +18,7 @@ def get_states():
     return jsonify(state_list)
 
 @app_views.route('/states/<state_id>', methods=['GET'], strict_slashes=False)
-def get_id(state_id=None):
+def get_state(state_id=None):
     state_dict = storage.all('State')
     try:
         return jsonify(state_dict['State' + "." + state_id].to_dict())
@@ -26,22 +26,37 @@ def get_id(state_id=None):
         abort(404)
 
 @app_views.route('/states/<state_id>', methods=['DELETE'], strict_slashes=False)
-def delete_id(state_id=None):
+def delete_state(state_id=None):
     try:
         obj = storage.get('State', state_id)
         storage.delete(obj)
         storage.save()
-    except:
-        abort(404)
+    except Exception as E:
+        print (E)
     return jsonify({}), 200
 
 @app_views.route('/states', methods=['POST'], strict_slashes=False)
-def post_states():
+def post_state():
     result = request.get_json()
     if not result:
         abort(400, {"message": "Not a JSON"})
     if not 'name' in result:
         abort(400, {"message": "Missing name"})
     obj = State(name = result['name'])
+    storage.new(obj)
+    storage.save()
+    return jsonify(obj.to_dict()), 201
+
+@app_views.route('/states/<state_id>', methods=['PUT'], strict_slashes=False)
+def put_state(state_id=None):
+    result = request.get_json()
+    if not result:
+        abort(400, {"message": "Not a JSON"})
+    obj = storage.get('State', state_id)
+    if obj == None:
+        abort(404)
+    for key, value in result.items():
+        if key != "id" and "created_at" and "updated_at":
+            setattr(obj, key, value)
     storage.save()
     return jsonify(obj.to_dict()), 201
