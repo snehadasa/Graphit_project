87ef29a1a28be49d96c96cf4ac0b2b8a7466b85f
David Kwan <dwkwan1989@gmail.com>
2019-09-11 21:19:53 +0000

    Import from states file

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -6,6 +6,7 @@ from flask import Flask, jsonify, abort, request
 from models import storage
 from api.v1.views import app_views
 from models.state import State
+from models.state import City
 from models.city import City
 import os
 app = Flask(__name__)
