5a3b4d61547193b9af8a63e760559cb005ef0c3e
snehadasa <sneha.dasasneha@gmail.com>
2019-09-13 20:46:08 +0000

    Revert "alter basemodel"
    This reverts commit 9f93d6c48c700ab83b517c675626404295e2b8ba.

--- models/base_model.py
+++ models/base_model.py
@@ -66,8 +66,6 @@ class BaseModel:
         if "updated_at" in new_dict:
             new_dict["updated_at"] = new_dict["updated_at"].strftime(time)
         new_dict["__class__"] = self.__class__.__name__
-        if "password" in new_dict:
-            del new_dict["password"]
         if "_sa_instance_state" in new_dict:
             del new_dict["_sa_instance_state"]
         return new_dict
