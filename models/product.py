e79e0f703a7ec7e29d7c7d4324469c3da3acbc6a
snehadasa <sneha.dasasneha@gmail.com>
2019-10-12 21:46:12 -0700

    cleanup

--- models/product.py
+++ models/product.py
@@ -10,13 +10,9 @@ from sqlalchemy.orm import relationship
 
 class Product(BaseModel, Base):
     """Representation of city """
-    if models.storage_t == "db":
-        __tablename__ = 'products'
-        product_id = Column(String(60), nullable=False)
-        price = Column(Float, nullable=False, default=0)
-    else:
-        product_id = ""
-        price = 0
+    __tablename__ = 'products'
+    product_id = Column(String(60), nullable=False)
+    price = Column(Float, nullable=False, default=0)
 
     def __init__(self, *args, **kwargs):
         """initializes Product"""
