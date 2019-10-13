db345f327e2d9724bc206c5077af13cf6679209c
snehadasa <sneha.dasasneha@gmail.com>
2019-10-12 21:32:34 -0700

    first commit

--- console.py
+++ console.py
@@ -4,17 +4,11 @@
 import cmd
 from datetime import datetime
 import models
-from models.amenity import Amenity
 from models.base_model import BaseModel
-from models.city import City
-from models.place import Place
-from models.review import Review
-from models.state import State
-from models.user import User
+from models.product import Product
 import shlex  # for splitting the line along spaces except in double quotes
 
-classes = {"Amenity": Amenity, "BaseModel": BaseModel, "City": City,
-           "Place": Place, "Review": Review, "State": State, "User": User}
+classes = { "BaseModel": BaseModel,  "Product": Product}
 
 
 class HBNBCommand(cmd.Cmd):
---
+++ models/product.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+""" holds class Product"""
+import models
+from models.base_model import BaseModel, Base
+from os import getenv
+import sqlalchemy
+from sqlalchemy import Column, String, ForeignKey, Float
+from sqlalchemy.orm import relationship
+
+
+class Product(BaseModel, Base):
+    """Representation of city """
+    if models.storage_t == "db":
+        __tablename__ = 'products'
+        product_id = Column(String(60), nullable=False)
+        price = Column(Float, nullable=False, default=0)
+    else:
+        product_id = ""
+        price = 0
+
+    def __init__(self, *args, **kwargs):
+        """initializes Product"""
+        super().__init__(*args, **kwargs)
